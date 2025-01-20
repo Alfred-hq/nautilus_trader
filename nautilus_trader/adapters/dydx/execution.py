34a5ca9134b0db574a6631ac2687628ad358961a
Utkarsh Shrivastava <ushrivastava@deqode.com>
2025-01-20 21:15:44 +0530

    fix: order status report

--- nautilus_trader/adapters/dydx/execution.py
+++ nautilus_trader/adapters/dydx/execution.py
@@ -372,7 +372,7 @@ class DYDXExecutionClient(LiveExecutionClient):
                 subaccount_number=self._subaccount,
                 symbol=instrument_id.symbol.value.removesuffix("-PERP"),
                 order_side=self._enum_parser.parse_nautilus_order_side(order_side),
-                order_type=self._enum_parser.parse_nautilus_order_type(order_type),
+                # order_type=self._enum_parser.parse_nautilus_order_type(order_type),
                 return_latest_orders=True,
             )
 
